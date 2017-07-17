aa61fabaa707807f70b8aa4858ca7b876cc9b553
anvien <anrial@126.com>
2017-07-17 16:39:40 +0800

    Add files via upload

--- hash.m
+++ hash.m
@@ -5,15 +5,20 @@ target_code = nonlinear_preprocess(password);
 rnn = rnn_run(rnn, target_code);
 
 rnn.output = fix(rnn.output * 10^(k+1));
-out = dec2hex(abs(int(rnn.output)));
-output_value = zeros(size(rnn.output_weights, 1), k);
-for i = 1 : k
-    output_value(:, i) = out(:,end-k+1);
-end
-hash_code = [];
-for i = 1 : length(output_value_1)
+out = cell(size(rnn.output_weights, 1), 1);
+output_value = cell(size(rnn.output_weights, 1), k);
+for i = 1 : size(rnn.output_weights, 1)
+    v = rnn.output(i);
+    out{i} = dec2hex(abs(v), 8);
     for j = 1 : k
-        hash_code = [hash_code output_value(i, k)];
+        output_value{i, j} = out{i}(end-k+j);
+    end
+end
+
+hash_code = [];
+for i = 1 : size(rnn.output_weights, 1)
+    for j = 1 : k
+        hash_code = [hash_code output_value(i, j)];
     end
 end
 
